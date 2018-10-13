3ebd494afbd546bf322edba684b328fe10322f71
Azeem Tariq <azeem@radixcode.com>
2018-10-13 19:37:45 +0500

    tut#4

--- index.js
+++ index.js
@@ -8,15 +8,28 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+const getVisitors = () => {
+  let clients = io.sockets.clients().connected;
+  let sockets = Object.values(clients);
+  let users = sockets.map(s => s.user);
+  return users;
+};
+
+const emitVisitors = () => {
+  io.emit("visitors", getVisitors());
+};
+
 io.on("connection", function(socket) {
   console.log("a user connected");
 
   socket.on("new_visitor", user => {
     console.log("new_visitor", user);
     socket.user = user;
+    emitVisitors();
   });
 
   socket.on("disconnect", function() {
+    emitVisitors();
     console.log("user disconnected");
   });
 });
