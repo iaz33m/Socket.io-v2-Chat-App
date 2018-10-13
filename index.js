e4298458416264ac802e66473db97f231968ab37
Azeem Tariq <azeem@radixcode.com>
2018-10-13 18:51:55 +0500

    tut#3

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require("express")();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
+const port = 6600;
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
@@ -9,11 +11,16 @@ app.get("/", function(req, res) {
 io.on("connection", function(socket) {
   console.log("a user connected");
 
+  socket.on("new_visitor", user => {
+    console.log("new_visitor", user);
+    socket.user = user;
+  });
+
   socket.on("disconnect", function() {
     console.log("user disconnected");
   });
 });
 
-http.listen(3001, function() {
-  console.log("listening on *:3001");
+http.listen(port, function() {
+  console.log(`listening on *:${port}`);
 });
